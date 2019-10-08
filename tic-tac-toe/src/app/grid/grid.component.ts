22ca2ae3e0b3123aeea6eaa2818957bf41dda692
Gcorp <ojasgupta4321@gmail.com>
2019-10-08 16:42:13 +1100

    Updated checkForWin

--- tic-tac-toe/src/app/grid/grid.component.ts
+++ tic-tac-toe/src/app/grid/grid.component.ts
@@ -9,12 +9,29 @@ import { of } from 'rxjs';
 export class GridComponent implements OnInit {
 
 
-  grid: string[][] = [["", "", ""], ["", "", ""], ["", "", ""]];
+  grid: string[][] = [["", "", ""], 
+                      ["", "", ""], 
+                      ["", "", ""]];
 
   playerPlaying: string = "X"
 
   checkForWin(){
+
     
+    for(let i = 0; i < 3; i++) {
+        if(this.grid[0][i] === this.grid[1][i] && this.grid[0][i] === this.grid[2][i] && (this.grid[0][i] !== "") ) {             // Vertical Check
+          alert(this.grid[i][0] + " Won.");
+        } 
+        else if (this.grid[i][0] === this.grid[i][1] && this.grid[i][0] === this.grid[i][2] && (this.grid[i][0] !== "")) {        // Horizontal Check
+          alert(this.grid[i][0] + " Won.");
+        }
+    }
+    // Diagonal Check
+    if(this.grid[0][0] === this.grid[1][1] && this.grid[0][0] === this.grid[2][2] && (this.grid[0][0] !== "")) {
+      alert(this.grid[0][0] + " Won.");
+    } else if (this.grid[0][2] === this.grid[1][1] && this.grid[0][2] === this.grid[2][0] && (this.grid[0][2] !== "")){
+      alert(this.grid[0][2] + " Won.");
+    }
   }
   boxClicked(x,y){
     this.grid[x][y] = this.playerPlaying;
